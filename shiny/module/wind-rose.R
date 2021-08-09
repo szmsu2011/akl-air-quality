b4ffc01042dac5eeadfc02e84138c285a92b34d5
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-08-10 01:31:45 +1200

    fix wind-rose legend

--- shiny/module/wind-rose.R
+++ shiny/module/wind-rose.R
@@ -1,29 +1,34 @@
 wind_rose_ui <- function(id) {
   ns <- NS(id)
 
-  column(
-    column(
-      tagList(echarts4rOutput(ns("wind_rose1"), height = "300px")),
-      tagList(echarts4rOutput(ns("wind_rose4"), height = "230px")),
-      tagList(echarts4rOutput(ns("wind_rose7"), height = "230px")),
-      tagList(echarts4rOutput(ns("wind_rose10"), height = "230px")),
-      width = 4
+  fluidPage(
+    fluidRow(
+      tagList(echarts4rOutput(ns("wind_rose0"), height = "30px"))
     ),
-    column(
-      tagList(echarts4rOutput(ns("wind_rose2"), height = "300px")),
-      tagList(echarts4rOutput(ns("wind_rose5"), height = "230px")),
-      tagList(echarts4rOutput(ns("wind_rose8"), height = "230px")),
-      tagList(echarts4rOutput(ns("wind_rose11"), height = "230px")),
-      width = 4
-    ),
-    column(
-      tagList(echarts4rOutput(ns("wind_rose3"), height = "300px")),
-      tagList(echarts4rOutput(ns("wind_rose6"), height = "230px")),
-      tagList(echarts4rOutput(ns("wind_rose9"), height = "230px")),
-      tagList(echarts4rOutput(ns("wind_rose12"), height = "230px")),
-      width = 4
-    ),
-    width = 12
+    fluidRow(
+      column(
+        tagList(echarts4rOutput(ns("wind_rose1"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose4"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose7"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose10"), height = "230px")),
+        width = 4
+      ),
+      column(
+        tagList(echarts4rOutput(ns("wind_rose2"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose5"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose8"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose11"), height = "230px")),
+        width = 4
+      ),
+      column(
+        tagList(echarts4rOutput(ns("wind_rose3"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose6"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose9"), height = "230px")),
+        tagList(echarts4rOutput(ns("wind_rose12"), height = "230px")),
+        width = 4
+      ),
+      width = 12
+    )
   )
 }
 
@@ -32,17 +37,13 @@ wind_rose_mod <- function(id, state) {
     pat_wd <- "(\\(|\\[)(\\d+),(\\d+)(\\])"
 
     e_wind_rose <- function(wind_data, yr, mth, loc) {
-      e <- (data <- wind_data %>%
+      data <- wind_data %>%
         filter(
           location == make_clean_names(loc),
           year(datetime) == yr,
           month(datetime) == mth
         ) %>%
         mutate(
-          # wind_dir = case_when(
-          #   wind_dir >= 180 ~ wind_dir - 180,
-          #   TRUE ~ wind_dir + 180
-          # ),
           wd = cut(wind_dir, seq(0, 360, 30), include.lowest = TRUE),
           wd = gsub(pat_wd, "% From \\2-\\3 deg", wd) %>%
             factor(gsub(pat_wd, "% From \\2-\\3 deg", levels(wd))),
@@ -57,11 +58,15 @@ wind_rose_mod <- function(id, state) {
         count(ws, wd, .drop = FALSE) %>%
         mutate(p = round(n / sum(n) * 100, 2)) %>%
         select(-n) %>%
-        pivot_wider(names_from = ws, values_from = p, values_fill = 0)) %>%
+        pivot_wider(names_from = ws, values_from = p, values_fill = 0)
+
+      if (mth == 0) data[-1] <- 11
+
+      e <- data %>%
         e_charts(wd) %>%
         e_angle_axis(wd, type = "category", axisLabel = list(formatter = "")) %>%
         e_radius_axis(max = 50, axisLabel = list(formatter = "{value}%")) %>%
-        e_polar(center = c("50%", ifelse(mth < 4, 200, "50%"))) %>%
+        e_polar(center = c("50%", ifelse(mth == 0, "300%", "50%"))) %>%
         e_tooltip(trigger = "axis")
 
       for (y in names(data)[-1]) {
@@ -69,28 +74,20 @@ wind_rose_mod <- function(id, state) {
           e_bar_(y, coord_system = "polar", stack = "stack")
       }
 
-      if (mth != 1L) {
-        e <- e %>%
-          e_group("grp2") %>%
-          e_connect_group("grp2")
-      } else {
-        e <- e %>%
-          e_group("grp2")
-      }
-
       e
     }
 
-    map(1:12, function(i) {
+    map(0:12, function(i) {
       output[["wind_rose" %>% paste0(i)]] <- renderEcharts4r({
         state[["data"]] %>%
           select(ws, wind_dir, location) %>%
           drop_na() %>%
           e_wind_rose(state[["year"]], i, state[["map_onclick"]]) %>%
-          e_legend(show = i == 3, right = 0, orient = "vertical") %>%
+          e_legend(show = i == 0) %>%
           e_title(
-            month(i, label = TRUE),
-            padding = ifelse(i < 4, 100, 15) %>% c(rep(0, 3))
+            show = mth != 0,
+            month(i + (i == 0), label = TRUE),
+            padding = c(15, rep(0, 3))
           )
       }) %>%
         bindCache(state[["year"]], state[["map_onclick"]], i)
