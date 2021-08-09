c39868ecc44a2ff11b6d9e2306225f32e8f1a506
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-08-10 00:19:44 +1200

    init migration from szmsu2011/hons-dissertation

---
+++ shiny/function.R
@@ -0,0 +1,25 @@
+Max <- function(x) {
+  x <- max(x, na.rm = TRUE)
+  ifelse(x > -Inf, round(x), NA)
+}
+
+fmt_date <- stamp("March 1, 1999")
+
+aqi_cat <- function(x) {
+  cut(x,
+    breaks = c(-Inf, 50, 100, 150, 200, 300, Inf),
+    labels = fct_inorder(c(
+      "Good", "Moderate", "Unhealthy for Sensitive",
+      "Unhealthy", "Very Unhealthy", "Hazardous"
+    ))
+  )
+}
+
+aqi_pal <- c(
+  "Good" = "#00E400",
+  "Moderate" = "#FFFF00",
+  "Unhealthy for Sensitive" = "#FF7E00",
+  "Unhealthy" = "#FF0000",
+  "Very Unhealthy" = "#8F3F97",
+  "Hazardous" = "#7E0023"
+)
---
+++ shiny/ini.R
@@ -0,0 +1,4 @@
+initial_app_state <- list(
+  data = append_data(NULL, "queen_street"),
+  map_onclick = "Queen Street"
+)
