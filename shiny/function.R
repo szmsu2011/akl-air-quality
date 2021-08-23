d0d8a20739d205becb3eaa7cea076eb84bd70f02
szmsu2011 <szmsu2011@hotmail.co.nz>
2021-08-24 02:48:47 +1200

    minor naming changes

--- shiny/function.R
+++ shiny/function.R
@@ -13,7 +13,7 @@ aqi_cat <- function(x) {
   cut(x,
     breaks = c(-Inf, 50, 100, 150, 200, 300, Inf),
     labels = fct_inorder(c(
-      "Good", "Moderate", "Unhealthy for Sensitive",
+      "Good", "Moderate", "Unhealthy for Sensitive Groups",
       "Unhealthy", "Very Unhealthy", "Hazardous"
     ))
   )
@@ -22,7 +22,7 @@ aqi_cat <- function(x) {
 aqi_pal <- c(
   "Good" = "#00E400",
   "Moderate" = "#FFFF00",
-  "Unhealthy for Sensitive" = "#FF7E00",
+  "Unhealthy for Sensitive Groups" = "#FF7E00",
   "Unhealthy" = "#FF0000",
   "Very Unhealthy" = "#8F3F97",
   "Hazardous" = "#7E0023"
